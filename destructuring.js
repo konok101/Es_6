5a235eab1c826732ffb6a9b004cdfc86bc5a250b
konok101 <rockykonok@gmail.com>
2022-04-11 03:02:57 +0600

    short use object

---
+++ destructuring.js
@@ -0,0 +1,8 @@
+const fish={ id: 21, name: "katla mash", phone: "01795598", 
+addres: 'Bogra',
+post:{ id: "first position", phone : 0202, color:'red'}
+}
+
+const { id, name, phone, color}= fish.post;
+
+console.log(color);
