8091d4384bcbcae6a42c1e4e5c1cd170eb49dd74
konok101 <rockykonok@gmail.com>
2022-04-13 01:57:37 +0600

    more map use for parameter arrow function

---
+++ more-map.js
@@ -0,0 +1,16 @@
+/* const friends=['konok majamder', 'akash','nur'];
+
+const stringLength= friends.map(n=>n.length);
+
+console.log(stringLength); */
+
+const products=[
+    {name:'pen', price:241, id:241},
+    {name:'book new', price:421, id:421},
+    {name:'Thirt', price:214, id:214}
+];
+
+//const names= products.map(x => x.name);
+// products.map(x=>console.log(x));
+products.forEach(x=>console.log(x));
+ 
